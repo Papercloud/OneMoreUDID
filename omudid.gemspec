85d8c3d58f8c49c619384853e450bdac82992ec6
David Lawson <tech.lawson@gmail.com>
2013-06-27 18:06:08 +1000

    Beginning implementing tests, travis

--- omudid.gemspec
+++ omudid.gemspec
@@ -21,6 +21,8 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
+  spec.add_development_dependency "vcr"
+  spec.add_development_dependency "webmock"
 
   spec.add_dependency "cupertino"
 
---
+++ test/test_omudid.rb
@@ -0,0 +1,36 @@
+require 'rubygems'
+
+require 'omudid'
+
+module Commander
+  module UI
+    attr :password
+  end
+end
+
+require 'terminal-table'
+require 'term/ansicolor'
+
+require 'cupertino_compatibility'
+
+require 'test/unit'
+require 'vcr'
+require 'omudid/portal_agent'
+
+VCR.configure do |c|
+  c.cassette_library_dir = 'test/vcr'
+  c.hook_into :webmock # or :fakeweb
+end
+
+class OneMoreUDIDTest < Test::Unit::TestCase
+  def test_teams
+    VCR.use_cassette('teams') do
+      agent = OneMoreUDID::PortalAgent.new
+      teams = agent.get_teams('test@email.com', 'password')
+
+      assert teams.count == 2
+      assert teams['XYZ1'] == 'Team 1'
+      assert teams['XYZ2'] == 'Team 2 - iOS Developer Program'
+    end
+  end
+end
